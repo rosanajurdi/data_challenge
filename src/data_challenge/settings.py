683c73db08305682f62198f81cc6153d41226eee
Rosana EL JURDI <44140190+rosanajurdi@users.noreply.github.com>
2026-01-16 13:00:30 +0100

    Add Kedro settings configuration

---
+++ src/data_challenge/settings.py
@@ -0,0 +1,9 @@
+"""Project settings."""
+
+from kedro.config import OmegaConfigLoader
+
+CONFIG_LOADER_CLASS = OmegaConfigLoader
+CONFIG_LOADER_ARGS = {
+    "base_env": "base",
+    "default_run_env": "local",
+}
